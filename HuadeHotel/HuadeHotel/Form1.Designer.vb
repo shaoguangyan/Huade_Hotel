43050dbdb05c4e44f2fc4459178bd6b2d46a2dd0
shaoguangyan <y.sg@foxmail.com>
2016-04-11 19:17:24 +0800

    Lip Test2

--- HuadeHotel/HuadeHotel/Form1.Designer.vb
+++ HuadeHotel/HuadeHotel/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,11 +35,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Lip Test "
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Lip Test "
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -47,5 +58,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- HuadeHotel/HuadeHotel/Form1.vb
+++ HuadeHotel/HuadeHotel/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class
